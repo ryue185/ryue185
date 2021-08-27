8727f43a544c48eb379ffa323600ab938881ab94
ryue185 <85585362+ryue185@users.noreply.github.com>
2021-08-27 11:43:57 -0500

    Add files via upload

---
+++ CEX.R
@@ -0,0 +1,61 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+food <- data.frame(matrix(ncol = 0, nrow = 1))
+house <- data.frame(matrix(ncol = 0, nrow = 1))
+for(year_counter in 0:18){
+  if(year_counter<10){
+    year_string<-paste("0",year_counter,sep="")
+  }else{
+    year_string<-year_counter
+  }
+  url<-paste("https://www.bls.gov/cex/pumd/data/comma/intrvw",year_string,".zip", sep="")
+  download.file(url,"mydata.zip")
+  unzip("mydata.zip")
+  file.remove("mydata.zip")
+  for(file_counter in 1:4){
+    if(year_counter<2|year_counter==17){
+      if(file_counter==1){
+        file_str<-paste("intrvw",year_string,"/intrvw",year_string,"/fmli",year_string,file_counter,"x.csv",sep="")
+      }else{
+        file_str<-paste("intrvw",year_string,"/intrvw",year_string,"/fmli",year_string,file_counter,".csv",sep="")
+      }
+    }else if(file_counter==1){
+      file_str<-paste("intrvw",year_string,"/fmli",year_string,file_counter,"x.csv",sep="")
+    }else{
+      file_str<-paste("intrvw",year_string,"/fmli",year_string,file_counter,".csv",sep="")
+    }
+    temp <- read_csv(file_str)
+    temp_h <-data.frame(temp$HOUSCQ)
+    colnames(temp_h)<-(paste(year_string,file_counter,sep="-"))
+    temp_f <-data.frame(temp$FOODCQ)
+    colnames(temp_f)<-(paste(year_string,file_counter,sep="-"))
+    house<-merge(house,temp_h,all=TRUE,by=0)
+    house<-subset(house,select=-c(Row.names))
+    food<-merge(food,temp_f,all=TRUE,by=0)
+    food<-subset(food,select=-c(Row.names))
+  }
+  unlink(paste("expn",year_string,sep=""),recursive = TRUE)
+  unlink(paste("intrvw",year_string,sep=""),recursive = TRUE)
+  unlink(paste("para",year_string,sep=""),recursive = TRUE)
+}
+
+food[food==0]<-NA
+house[house==0]<-NA
+food_mean<-data.frame(colMeans(food, na.rm = TRUE))
+colnames(food_mean)<-c("mean_food_spending")
+food_mean$quarter<-rownames(food_mean)
+house_mean<-data.frame(colMeans(house, na.rm = TRUE))
+colnames(house_mean)<-c("mean_housing_spending")
+house_mean$quarter<-rownames(house_mean)
+
+ggplot(data=house_mean,aes(x=quarter,y=mean_housing_spending))+
+  geom_point(shape=8)+
+  ylab("CEX average housing spending") + xlab("Quarter")+
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplot(data=food_mean,aes(x=quarter,y=mean_food_spending))+
+  geom_point(shape=9)+
+  ylab("CEX average food spending") + xlab("Quarter")+
+  theme(axis.text.x = element_text(angle = 90))
